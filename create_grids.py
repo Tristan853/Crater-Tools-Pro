35ff74c673bcd13b761b093832e166af969adcf1
Tristan853 <luginbillt1@gmail.com>
2024-06-20 13:29:01 -0400

    modified:   create_grids.py

--- create_grids.py
+++ create_grids.py
@@ -4,23 +4,19 @@ import arcpy.management
 input_polygon = arcpy.GetParameterAsText(0)
 grid_size = arcpy.GetParameterAsText(1)
 output_location = arcpy.GetParameterAsText(2)
-
+intGrid = int(grid_size)
+grid_conversion = (intGrid * 1000)
 
 
 aprx = arcpy.mp.ArcGISProject("CURRENT")
 active_map = aprx.activeMap
 arcpy.env.overwriteOutput = True
 arcpy.env.workspace = output_location
+desc = arcpy.Describe(input_polygon)
+arcpy.env.outputCoordinateSystem = desc.spatialReference
 
 layer_name = input_polygon.split('\\')
-origin = arcpy.management.CalculateGeometryAttributes(input_polygon, [['Center_X', 'CENTROID_X'],['Center_Y', 'CENTROID_Y']], '', '', '', 'DD')
-field = ['Center_X', 'Center_Y']
-with arcpy.da.SearchCursor(origin, field) as cursor:
-    for row in cursor:
-        xy = row
-coord = str(xy[0]) + ' ' + str(xy[1])
-newYcoord = xy[1] + 0.05
-ycoord = str(xy[0]) + ' ' + str(newYcoord)
-grid = arcpy.management.CreateFishnet(output_location + r'\Grid_' + layer_name[-1].split('_')[-1], coord, ycoord, grid_size, grid_size, '0', '0', '', 'NO_LABELS', 'MINOF', 'POLYGON')
+extent = desc.extent
+grid = arcpy.management.CreateFishnet(output_location + r'\Grid_' + layer_name[-1].split('_')[-1], str(extent.XMin) + " " + str(extent.YMin), str(extent.XMin) + " " + str(extent.YMax), grid_conversion, grid_conversion, number_rows='0', number_columns='0',corner_coord=str(extent.XMax) + " " + str(extent.YMax) , labels="NO_LABELS", template='#', geometry_type='POLYGON')
 
 active_map.addDataFromPath(grid)
